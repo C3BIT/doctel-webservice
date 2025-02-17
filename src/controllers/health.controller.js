01c3f8003e1dd80950b9bbd132c53adc3617a284
Nahid Hasan <nahidjc80@gmail.com>
2025-02-17 12:22:07 +0600

    sequelize added

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -1,22 +1,22 @@
 const getHealth = async (req, res) => {
-    try {
-      const healthStatus = {
-        status: "success",
-        message: "Health is OK",
-        data: {
-          uptime: process.uptime(),
-          timestamp: new Date().toISOString(),
-        },
-      };
-      res.success(healthStatus);
-    } catch (error) {
-      res.error({
-        status: "error",
-        message: "Internal Server Error",
-      });
-    }
-  };
-  
-  module.exports = {
-    getHealth,
-  };
+  try {
+    const healthStatus = {
+      status: "success",
+      message: "Health is OK",
+      data: {
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+      },
+    };
+    res.success(healthStatus);
+  } catch (error) {
+    res.error({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+};
+
+module.exports = {
+  getHealth,
+};
