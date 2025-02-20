a0707d453fde80614e5eee6a9d484990fea63895
Nahid Hasan <nahidjc80@gmail.com>
2025-02-20 23:01:17 +0600

    implement Authorization (JWT-based) Middleware for Accessing Private APIs for User

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,7 +3,8 @@ const { errorResponseHandler } = require("./errorResponseHandler.js");
 const { jwtSecret } = require("../configs/variables.js");
 const { statusCodes } = require("../utils/statusCodes.js");
 
-const isTokenExpired = (expirationTime) => expirationTime <= Math.floor(Date.now() / 1000);
+const isTokenExpired = (expirationTime) =>
+  expirationTime <= Math.floor(Date.now() / 1000);
 
 const authenticateDoctor = (req, res, next) => {
   try {
@@ -52,4 +53,63 @@ const authenticateDoctor = (req, res, next) => {
   }
 };
 
-module.exports = { authenticateDoctor };
+const authenticatePatient = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1] || req.headers.token;
+    if (!token) {
+      throw Object.assign(new Error(), {
+        status: statusCodes.UNAUTHORIZED,
+        error: {
+          code: 40113,
+          reason: errorCodes[40113].reason,
+          message: errorCodes[40113].message,
+        },
+      });
+    }
+
+    try {
+      const decoded = jsonwebtoken.verify(token, jwtSecret);
+
+      if (isTokenExpired(decoded.exp)) {
+        throw Object.assign(new Error(), {
+          status: statusCodes.UNAUTHORIZED,
+          error: {
+            code: 40110,
+          },
+        });
+      }
+
+      if (decoded.role !== "patient") {
+        throw Object.assign(new Error(), {
+          status: statusCodes.UNAUTHORIZED,
+          error: {
+            code: 40114,
+          },
+        });
+      }
+
+      req.user = decoded;
+      return next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        throw Object.assign(new Error(), {
+          status: statusCodes.UNAUTHORIZED,
+          error: {
+            code: 40110,
+          },
+        });
+      } else {
+        throw Object.assign(new Error(), {
+          status: statusCodes.UNAUTHORIZED,
+          error: {
+            code: 40111,
+          },
+        });
+      }
+    }
+  } catch (err) {
+    errorResponseHandler(err, req, res);
+  }
+};
+
+module.exports = { authenticateDoctor, authenticatePatient };
