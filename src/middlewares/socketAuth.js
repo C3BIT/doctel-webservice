0fd163487caeb327d3f69869fbf0428e703a0825
Nahid Hasan <nahidjc80@gmail.com>
2025-03-08 18:45:06 +0600

    Backend:: Implement WebSocket Connection for patient and doctor

--- src/middlewares/socketAuth.js
+++ src/middlewares/socketAuth.js
@@ -1,21 +1,23 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateSocket = async (socket, next) => {
-    const token = socket.handshake.headers.token;
+    const token = socket.handshake.headers.token || 
+    socket.handshake.query.token || 
+    socket.handshake.headers.authorization?.split(" ")[1];
     if (!token) {
         return next(new Error("Authentication error: No token provided"));
     }
-
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         socket.user = {
             id: decoded.id,
             role: decoded.role,
         };
-
         next();
     } catch (error) {
-        next(new Error("Authentication error: Invalid token"));
+        console.log("🚨 Token Expired, notifying client...");
+        socket.emit("token_expired");
+        socket.disconnect();
     }
 };
 
