9f6af626ded94764cc5844aa9a59b61fd603c66e
Nahid Hasan <nahidjc80@gmail.com>
2025-03-09 22:49:02 +0600

    issue resolved

--- src/middlewares/socketAuth.js
+++ src/middlewares/socketAuth.js
@@ -1,24 +1,25 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateSocket = async (socket, next) => {
-    const token = socket.handshake.headers.token || 
-    socket.handshake.query.token || socket.handshake.query.token||
+  const token =
+    socket.handshake.headers.token ||
+    socket.handshake.query.token ||
     socket.handshake.headers.authorization?.split(" ")[1];
-    if (!token) {
-        return next(new Error("Authentication error: No token provided"));
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        socket.user = {
-            id: decoded.id,
-            role: decoded.role,
-        };
-        next();
-    } catch (error) {
-        console.log("🚨 Token Expired, notifying client...");
-        socket.emit("token_expired");
-        socket.disconnect();
-    }
+  if (!token) {
+    return next(new Error("Authentication error: No token provided"));
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    socket.user = {
+      id: decoded.id,
+      role: decoded.role,
+    };
+    next();
+  } catch (error) {
+    console.log("🚨 Token Expired, notifying client...");
+    socket.emit("token_expired");
+    socket.disconnect();
+  }
 };
 
 module.exports = { authenticateSocket };
